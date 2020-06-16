5502dc90fd30ad9f93eb7836d977872dcaa8a640
sreskala <sam.sagas@hotmail.com>
2020-06-15 21:01:38 -0400

    setup initial application and homepage

---
+++ src/components/pages/homepage/Homepage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+//styles
+import './homepage.styles.scss';
+
+const Homepage = () => {
+
+    return(
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span classNAme="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span classNAme="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Homepage;
